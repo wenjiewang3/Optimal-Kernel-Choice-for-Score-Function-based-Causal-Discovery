fd7c676ffde0fe2d269427caf2bdb09ae1d6b6c5
Wenjie Wang <167825818+wenjiewang3@users.noreply.github.com>
2024-07-14 20:00:36 +1000

    Delete OptimalScore directory

--- OptimalScore/run.sh
+++
@@ -1,5 +0,0 @@
-# run GES with Conditional Marginal likelihood as score function
-python main.py --data_type con --score Marg --n 300 --gd 0.5 --device cpu
-
-# run GES with Mutual Information-based score function
-#python main.py --data_type con --score MI --n 300 --gd 0.5 --device cpu
